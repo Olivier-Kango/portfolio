d540877d69a971c6310954ac76c7c28eb18bd337
derrick <djmulinde17@gmail.com>
2022-09-10 14:41:52 +0300

    Add both error and success message formats

--- validation.js
+++ validation.js
@@ -13,43 +13,51 @@ const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 fname.addEventListener('input', () => {
-  if (fname.value.length < 30) {
+  if (fname.value.length !== 0 && fname.value.length < 20) {
     errorName.textContent = '';
+    document.getElementById('field-name').setAttribute('class', 'success');
   } else {
     errorName.textContent = 'Name should be less than 30 characters.';
+    document.getElementById('field-name').setAttribute('class', 'error');
   }
 });
 
 email.addEventListener('input', () => {
   if (email.value === email.value.toLowerCase()) {
     errorEmail.textContent = '';
+    document.getElementById('field-email').setAttribute('class', 'success');
   } else {
     errorEmail.textContent = 'Email Address should not have uppercase letters.';
+    document.getElementById('field-email').setAttribute('class', 'error');
   }
 
   if (!emailRegex.test(email.value)) {
     errorEmail.textContent = 'Invalid Email Address';
+    document.getElementById('field-email').setAttribute('class', 'error');
   }
 });
 
 message.addEventListener('input', () => {
   if (message.value.length < 10) {
     errorMsg.textContent = 'Message should have more 10 characters.';
+    document.getElementById('field-text').setAttribute('class', 'error');
   } else {
     errorMsg.textContent = '';
+    document.getElementById('field-text').setAttribute('class', 'success');
   }
 
   if (message.value.length > 400) {
     errorMsg.textContent = 'Message should not exceed 400 characters.';
+    document.getElementById('field-text').setAttribute('class', 'error');
   }
 });
 
 form.addEventListener('submit', (event) => {
-  // eslint-disable-next-line linebreak-style
   event.preventDefault();
 
   if (fname.value === '' && email.value === '' && message.value === '') {
     overralError.textContent = 'Fill in entire form before submitting.';
+    document.getElementById('field').setAttribute('class', 'error');
   } else {
     overralError.textContent = '';
   }
