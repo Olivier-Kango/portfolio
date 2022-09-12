bbd54b9ab2b9a4c1ae5840186bb252682fd78ae3
Olivier-Kango <olivierkango@gmail.com>
2022-09-12 12:18:46 -0700

    Fix some linter errors

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const navMenu = document.querySelector('.li-desk');
 const bars = () => {
   hamburger.classList.toggle('change');
   navMenu.classList.toggle('change');
-}
+};
 
 hamburger.addEventListener('click', bars);
 document.querySelectorAll('.nav-link').forEach((element) => element.addEventListener('click', () => {
