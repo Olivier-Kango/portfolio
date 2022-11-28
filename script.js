a3fc15da3f661889adb1e00a21fc03344e4f26cf
Olivier-Kango <olivierkango@gmail.com>
2022-11-28 12:28:10 -0800

    Fix bug on space-travelers hub

--- script.js
+++ script.js
@@ -138,8 +138,8 @@ const listProjects = [
       featureOne: 'See Live &nbsp; <i class="fa fa-arrow-circle-right"></i>',
       featureTwo: 'See Source &nbsp; <i class="fa fa-github"></i>',
       bgpopup: "url('./assets/images/Snapshoot Portfolio-9.png')",
-      webSite: "window.open('https://spacetravelersod.netlify.app/' '_blank');",
-      gitHub: "window.open('https://github.com/Olivier-Kango/space-travelers-hub' '_blank');",
+      webSite: "window.open('https://spacetravelersod.netlify.app/', '_blank');",
+      gitHub: "window.open('https://github.com/Olivier-Kango/space-travelers-hub', '_blank');",
     },
   },
 ];
