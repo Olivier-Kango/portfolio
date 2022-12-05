c502ebfdb8944a3b2e5a02abb0d23ee3a2713782
Olivier-Kango <olivierkango@gmail.com>
2022-12-05 13:42:05 -0800

    Add double quote to math magician

--- script.js
+++ script.js
@@ -54,14 +54,14 @@ const listProjects = [
   },
   {
     name: 'Math magicians',
-    description: "'Math magicians' is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations and Read a random math-related quote.",
+    description: '"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations and Read a random math-related quote.',
     skills: ['React', 'CSS'],
     feature: 'See Project',
     bgDivOne: "url('assets/images/work-img-7.png')",
     listPopupProjects: {
       popName: 'Math magicians',
       popButton: '&times;',
-      popDescription: "'Math magicians' is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations",
+      popDescription: '"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations',
       popSkills: ['React', 'CSS'],
       featureOne: 'See Live &nbsp; <i class="fa fa-arrow-circle-right"></i>',
       featureTwo: 'See Source &nbsp; <i class="fa fa-github"></i>',
