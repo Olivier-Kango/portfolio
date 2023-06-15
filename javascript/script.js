61baee6bcdc1037d61c368ab5105559ae3937fb4
Olivier-Kango <olivierkango@gmail.com>
2023-06-15 16:50:53 +0300

    Change image PSS

--- javascript/script.js
+++ javascript/script.js
@@ -29,7 +29,7 @@ const listProjects = [
       popSkills: ['RoR', 'React.js', 'Redux', 'SCSS'],
       featureOne: 'See Live &nbsp; <i class="fa fa-arrow-circle-right"></i>',
       featureTwo: 'See Source &nbsp; <i class="fa fa-github"></i>',
-      bgpopup: "url('./assets/images/work-img-16.png')",
+      bgpopup: "url('./assets/images/Snapshoot Portfolio-16.png')",
       webSite: "window.open('https://pss-digital.netlify.app/', '_blank');",
       gitHub: "window.open('https://github.com/Olivier-Kango/pss-digital-backend', '_blank');",
     },
