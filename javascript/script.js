096ecc4ededba790c706b3e5eeac5faafd51eba1
Olivier-Kango <olivierkango@gmail.com>
2023-08-19 17:12:21 +0300

    change domain eglise cfcm goma

--- javascript/script.js
+++ javascript/script.js
@@ -66,7 +66,7 @@ const listProjects = [
       featureOne: 'See Live &nbsp; <i class="fa fa-arrow-circle-right"></i>',
       featureTwo: 'See Source &nbsp; <i class="fa fa-github"></i>',
       bgpopup: "url('./assets/images/Snapshoot Portfolio-13.png')",
-      webSite: "window.open('https://cfcm.netlify.app/', '_blank');",
+      webSite: "window.open('www.eglisecfcm.org/', '_blank');",
       gitHub: "window.open('https://github.com/Olivier-Kango/cfcm', '_blank');",
     },
   },
